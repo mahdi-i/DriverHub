d833b57e83ec4dbeea1133094c9418f3c814f8b7
Mahdi <mahdibagherichanel@gmail.com>
2026-05-04 17:08:52 +0330

    add enum files for status day and appinment

---
+++ apps/api/src/common/enums/week-day.enum.ts
@@ -0,0 +1,9 @@
+export enum DaysOfWeek {
+  SATURDAY = 'saturday',
+  SUNDAY = 'sunday',
+  MONDAY = 'monday',
+  TUESDAY = 'tuesday',
+  WEDNESDAY = 'wednesday',
+  THURSDAY = 'thursday',
+  FRIDAY = 'friday',
+}
--- apps/api/src/modules/dashboard-driver/modules/appointment-driver/appointment-driver.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppointmentDriverService } from './appointment-driver.service';
-import { AppointmentDriverController } from './appointment-driver.controller';
-
-@Module({
-  controllers: [AppointmentDriverController],
-  providers: [AppointmentDriverService],
-})
-export class AppointmentDriverModule {}
