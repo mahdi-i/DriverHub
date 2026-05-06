f1b197aa3e62c489f6bffe5d7a160c58a754a75b
Mahdi <mahdibagherichanel@gmail.com>
2026-05-06 12:56:34 +0330

    complet HeroImageMainPage

---
+++ apps/web/src/core/features/main/components/ui/hero-img/HeroImageMainPage.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ImgNormalCustom } from "@/core/components/custom/ui/image/ImgNormalCustom";
+import { useIsMobile } from "@/core/hooks/useIsMobile";
+
+function HeroImageMainPage() {
+  const isMobile = useIsMobile();
+  if (isMobile) return null;
+
+  return (
+    <section className="w-full flex justify-center items-center">
+      <ImgNormalCustom
+        src={"/img/main/home/hero-824e4df4.webp"}
+        alt="hero img"
+        width={2000}
+        height={50}
+      />
+    </section>
+  );
+}
+
+export default HeroImageMainPage;
