ee3087835e0fa2259766e5e1e22e27991848e6b8
Mahdi <mahdibagherichanel@gmail.com>
2026-05-06 11:33:03 +0330

    refactor PartialType for all update dto

--- apps/api/src/modules/auth/dto/update-auth.dto.ts
+++ apps/api/src/modules/auth/dto/update-auth.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
+
 import { CreateAuthDto } from './create-phone.dto';
 
 export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
--- apps/api/src/modules/dashboard-trainee/modules/appointment-requests/dto/update-appointment_request.dto.ts
+++ apps/api/src/modules/dashboard-trainee/modules/appointment-requests/dto/update-appointment_request.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
+
 import { CreateAppointmentRequestDto } from './create-appointment_request.dto';
 
 export class UpdateAppointmentRequestDto extends PartialType(
