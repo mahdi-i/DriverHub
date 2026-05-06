ac280f9afcd82c660f8a1ea8637a64bf3a8321d8
Mahdi <mahdibagherichanel@gmail.com>
2026-05-06 21:51:50 +0330

    refactor layout pages for main DynamicSearch

--- apps/web/src/app/(pages)/layout.tsx
+++ apps/web/src/app/(pages)/layout.tsx
@@ -1,5 +1,10 @@
+import SectionLayout from "@/core/components/custom/ui/wrapper/SectionLayout";
+import { Card } from "@/core/components/shadcn/ui/card/card";
+import { DynamicSearchForm } from "@/core/features/booking/components/blocks/search/DynamicSearchForm";
 import Footer from "@/core/features/main/components/blocks/footer/Footer";
 import Header from "@/core/features/main/components/blocks/header/Header";
+import { LicenseFilter } from "@/core/features/main/components/ui/filter/LicenseFilter";
+import HeroImageMainPage from "@/core/features/main/components/ui/hero-img/HeroImageMainPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,6 +21,14 @@ async function layout({
   return (
     <main>
       <Header />
+
+      <HeroImageMainPage />
+      <SectionLayout>
+        <Card className="py-0 ">
+          <LicenseFilter />
+          <DynamicSearchForm />
+        </Card>
+      </SectionLayout>
       {children}
       <Footer />
     </main>
