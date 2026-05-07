391a24930cb8e4089d54573963eac66e51091b55
Mahdi <mahdibagherichanel@gmail.com>
2026-05-07 16:28:39 +0330

    remove ws

--- apps/api/src/common/modules/ws.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WsGateway } from '@shared/services/ws.gateway.service';
-
-@Module({
-  providers: [WsGateway],
-})
-export class WsModule {}
--- apps/api/src/common/services/chart-data.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/ws/dto/chart-data.dto.ts
-export class ChartDataDto {
-  name: string;
-  price: number;
-}
