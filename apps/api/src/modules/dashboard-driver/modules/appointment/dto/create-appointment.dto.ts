4f99b570cdda5d80a845f2890a9fbdba54045784
Mahdi <mahdibagherichanel@gmail.com>
2026-05-09 20:23:02 +0330

    remove Appointment module and AppointmentRequestsModule

--- apps/api/src/modules/dashboard-driver/modules/appointment/dto/create-appointment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsString } from 'class-validator';
-
-export class CreateAppointmentDto {
-  @IsString()
-  note: string;
-
-  @IsNumber()
-  score: number;
-}
--- apps/api/src/modules/dashboard-trainee/modules/appointment-requests/dto/update-appointment_request.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-
-import { CreateAppointmentRequestDto } from './create-appointment_request.dto';
-
-export class UpdateAppointmentRequestDto extends PartialType(
-  CreateAppointmentRequestDto,
-) {}
