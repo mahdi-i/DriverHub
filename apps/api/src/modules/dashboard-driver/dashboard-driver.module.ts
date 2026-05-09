e7b91b9d62f0ccb4bda0e9109b2440484d5e0d0a
Mahdi <mahdibagherichanel@gmail.com>
2026-05-09 20:27:44 +0330

    refactor DashboardDriverModule and AnalysisModule

--- apps/api/src/modules/dashboard-driver/dashboard-driver.module.ts
+++ apps/api/src/modules/dashboard-driver/dashboard-driver.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { AppointmentRequest } from '@core/dashboard-trainee/modules/appointment-requests/entities/appointment-request.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalysisModule } from './modules/analysis/analysis.module';
-import { AppointmentModule } from './modules/appointment/appointment.module';
 import { DriverModule } from './modules/driver/driver.module';
 import { Driver } from './modules/driver/entities/driver.entity';
 import { ProfileDriver } from './modules/profile/entities/profile.entity';
@@ -13,16 +11,10 @@ import { ScheduleDriverModule } from './modules/schedule-driver/schedule-driver.
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      ProfileDriver,
-      ScheduleDriver,
-      Driver,
-      AppointmentRequest,
-    ]),
+    TypeOrmModule.forFeature([ProfileDriver, ScheduleDriver, Driver]),
     ProfileModule,
     DriverModule,
     ScheduleDriverModule,
-    AppointmentModule,
     AnalysisModule,
   ],
 })
