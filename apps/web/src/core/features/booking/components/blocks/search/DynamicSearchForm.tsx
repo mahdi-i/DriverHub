a1830cbced8578171a055099e21308449602dd47
Mahdi <mahdibagherichanel@gmail.com>
2026-05-10 11:39:12 +0330

    refactor dynamic serch components for mobile size

--- apps/web/src/core/features/booking/components/blocks/search/DynamicSearchForm.tsx
+++ apps/web/src/core/features/booking/components/blocks/search/DynamicSearchForm.tsx
@@ -2,7 +2,8 @@
 
 import { usePathname } from "next/navigation";
 
-import { useState } from "react";
+import { useIsMobile } from "@/core/hooks/useIsMobile";
+import { useMemo, useState } from "react";
 import CalendarBooking from "../../ui/filter/calendar/CalendarBooking";
 import DynamicSearchSubmitBtn from "../../ui/filter/DynamicSearchSubmitBtn";
 import DynamicSearchCarModle from "../../ui/filter/form/DynamicSearchCarModle";
@@ -11,49 +12,72 @@ import DynamicSearchMotorcycle from "../../ui/filter/form/DynamicSearchMotorcycl
 import DynamicSearchPlace from "../../ui/filter/form/DynamicSearchPlace";
 import DynamicSearchVehicleType from "../../ui/filter/form/DynamicSearchVehicleType";
 
-export function DynamicSearchForm() {
+export function DynamicSearchForm({ id }: { id?: string }) {
   const pathname = usePathname();
   const [valueCalendar, setValueCalendar] = useState<Date | null>(null);
+  const isMobile = useIsMobile();
 
-  const isCar = pathname.includes("/");
-  const isMotorcycle = pathname.includes("/motorcycle");
-  const isHeavyMotor = pathname.includes("/heavy-motorcycle");
-  const isBase1 = pathname.includes("/car-base1");
-  const isBase2 = pathname.includes("/car-base2");
-  const isBase3 = pathname.includes("/car-base3");
+  const routeInfo = useMemo(() => {
+    console.log(id, "id");
+    if (id !== undefined) {
+      return {
+        isCar: id === "",
+        isMotorcycle: id === "motorcycle",
+        isHeavyMotor: id === "heavy-motorcycle",
+        isBase1: id === "car-base1",
+        isBase2: id === "car-base2",
+        isBase3: id === "car-base3",
+      };
+    }
 
+    return {
+      isCar: pathname === "/" || pathname === "",
+      isMotorcycle: pathname.includes("/motorcycle"),
+      isHeavyMotor: pathname.includes("/heavy-motorcycle"),
+      isBase1: pathname.includes("/car-base1"),
+      isBase2: pathname.includes("/car-base2"),
+      isBase3: pathname.includes("/car-base3"),
+    };
+  }, [id, pathname]);
   return (
     <div>
-      <div className=" pb-4 px-4 space-y-4">
-        <div className="flex gap-3 items-end">
+      <div className=" pb-4 px-4 space-y-4 mt-2">
+        <div
+          className={
+            isMobile ? "space-y-4" : "space-y-4 gap-3 items-center flex"
+          }
+        >
           <CalendarBooking
             valueCalendar={valueCalendar}
             setValueCalendar={setValueCalendar}
           />
           <DynamicSearchGender />
           <DynamicSearchPlace
-            isCar={isCar}
-            isHeavyMotor={isHeavyMotor}
-            isMotorcycle={isMotorcycle}
+            isBase1={routeInfo.isBase1}
+            isBase2={routeInfo.isBase2}
+            isBase3={routeInfo.isBase3}
+            isCar={routeInfo.isCar}
+            isHeavyMotor={routeInfo.isHeavyMotor}
+            isMotorcycle={routeInfo.isMotorcycle}
           />
           <DynamicSearchCarModle
-            isCar={isCar}
-            isHeavyMotor={isHeavyMotor}
-            isMotorcycle={isMotorcycle}
+            isCar={routeInfo.isCar}
+            isHeavyMotor={routeInfo.isHeavyMotor}
+            isMotorcycle={routeInfo.isMotorcycle}
           />
           <DynamicSearchMotorcycle
-            isHeavyMotor={isHeavyMotor}
-            isMotorcycle={isMotorcycle}
+            isHeavyMotor={routeInfo.isHeavyMotor}
+            isMotorcycle={routeInfo.isMotorcycle}
           />
-          <DynamicSearchVehicleType isBase1={isBase1} />
+          <DynamicSearchVehicleType isBase1={routeInfo.isBase1} />
 
           <DynamicSearchSubmitBtn
-            isBase1={isBase1}
-            isBase2={isBase2}
-            isBase3={isBase3}
-            isCar={isCar}
-            isHeavyMotor={isHeavyMotor}
-            isMotorcycle={isMotorcycle}
+            isBase1={routeInfo.isBase1}
+            isBase2={routeInfo.isBase2}
+            isBase3={routeInfo.isBase3}
+            isCar={routeInfo.isCar}
+            isHeavyMotor={routeInfo.isHeavyMotor}
+            isMotorcycle={routeInfo.isMotorcycle}
           />
         </div>
       </div>
