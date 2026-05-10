f3d3850e1730b6b355344f2235b9ca8256c212b4
Mahdi <mahdibagherichanel@gmail.com>
2026-05-11 00:10:26 +0330

    add hero section

---
+++ apps/web/src/core/features/main/components/ui/hero-section/HeroSection.tsx
@@ -0,0 +1,27 @@
+import SectionLayout from "@/core/components/custom/ui/wrapper/SectionLayout";
+import { Card } from "@/core/components/shadcn/ui/card/card";
+import { DynamicSearchForm } from "@/core/features/booking/components/blocks/search/DynamicSearchForm";
+import { LicenseFilter } from "../filter/LicenseFilter";
+import HeroImageMainPage from "../hero-img/HeroImageMainPage";
+import MapSectionMainPage from "../home-page/MapSectionMainPage";
+
+function HeroSection() {
+  return (
+    <>
+      <section className="relative w-full mb-35 hidden md:flex">
+        <HeroImageMainPage />
+        <div className="absolute -bottom-59 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-300  z-10">
+          <Card className="w-full py-4 ">
+            <LicenseFilter />
+            <DynamicSearchForm />
+          </Card>
+        </div>
+      </section>
+      <SectionLayout>
+        <MapSectionMainPage />
+      </SectionLayout>
+    </>
+  );
+}
+
+export default HeroSection;
