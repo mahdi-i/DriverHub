a78fda979229ba05e60b386a0a09e6c321e7ca16
Mahdi <mahdibagherichanel@gmail.com>
2026-05-10 12:12:40 +0330

    improve css stype dashboard

--- apps/web/src/core/components/custom/ui/wrapper/ClientLayout.tsx
+++ apps/web/src/core/components/custom/ui/wrapper/ClientLayout.tsx
@@ -12,20 +12,16 @@ export default function ClientLayout({
 
   return (
     <main className="flex h-screen bg-[#0f172a] relative overflow-hidden">
-      {/* پس‌زمینه گرادیانت با رنگ‌های سایت */}
-      <div className="absolute inset-0 bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a]" />
+      <div className="absolute inset-0 bg-linear-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a]" />
 
-      {/* نورهای پس‌زمینه */}
-      <div className="absolute top-0 left-0 w-[500px] h-[500px] bg-[#fdb913]/10 rounded-full blur-[150px] -translate-x-1/2 -translate-y-1/2 pointer-events-none" />
-      <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-[#0077db]/25 rounded-full blur-[150px] translate-x-1/2 translate-y-1/2 pointer-events-none" />
+      <div className="absolute top-0 left-0 w-125 h-125 bg-[#fdb913]/10 rounded-full blur-[150px] -translate-x-1/2 -translate-y-1/2 pointer-events-none" />
+      <div className="absolute bottom-0 right-0 w-125 h-125 bg-[#0077db]/25 rounded-full blur-[150px] translate-x-1/2 translate-y-1/2 pointer-events-none" />
 
       <MainSidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       <section className="flex-1 flex flex-col overflow-hidden">
-        {/* هدر */}
         <MainHeader setSidebarOpen={setSidebarOpen} />
 
-        {/* محتوا */}
         <div className="flex-1 overflow-y-auto">{children}</div>
       </section>
     </main>
