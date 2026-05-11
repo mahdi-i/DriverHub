0e6d09a1e8d3c9f9b739a7c30fc588d9b5ad7fac
Mahdi <mahdibagherichanel@gmail.com>
2026-05-11 18:09:12 +0330

    add date fake for schedules

---
+++ apps/web/src/core/features/driver/assets/mock/schedules.ts
@@ -0,0 +1,33 @@
+export const schedules = [
+  {
+    day: "شنبه",
+    shift1: { start: "08:00", end: "12:00" },
+    shift2: { start: "16:00", end: "20:00" },
+  },
+  {
+    day: "یکشنبه",
+    shift1: { start: "09:00", end: "13:00" },
+    shift2: { start: "17:00", end: "21:00" },
+  },
+  {
+    day: "دوشنبه",
+    shift1: { start: "10:00", end: "15:00" },
+  },
+  {
+    day: "سه‌شنبه",
+    shift1: { start: "08:30", end: "14:30" },
+    shift2: { start: "15:30", end: "19:30" },
+  },
+  {
+    day: "چهارشنبه",
+    shift1: { start: "09:00", end: "12:30" },
+  },
+  {
+    day: "پنجشنبه",
+    shift1: { start: "08:00", end: "13:00" },
+    shift2: { start: "14:00", end: "18:00" },
+  },
+  {
+    day: "جمعه",
+  },
+];
