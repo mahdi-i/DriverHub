dd9137399c16a6c6d1d43ac7eae8e3a0775d4c8c
Mahdi <mahdibagherichanel@gmail.com>
2026-05-12 14:35:53 +0330

    add fake data authdatafake

---
+++ apps/web/src/core/features/auth/assets/mock/authdatafake.tsx
@@ -0,0 +1,15 @@
+import { Car, UserCheck } from "lucide-react";
+export const authdatafake = [
+  {
+    id: "driver",
+    title: "راننده",
+    description: "من راننده هستم و می‌خواهم ثبت‌نام کنم",
+    icon: <Car />,
+  },
+  {
+    id: "learner",
+    title: "درخواست‌کننده گواهینامه",
+    description: "من قصد دارم برای گواهینامه اقدام کنم",
+    icon: <UserCheck />,
+  },
+];
