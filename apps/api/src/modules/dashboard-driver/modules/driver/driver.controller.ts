dab50c5f6017a1485e95a6334d2e1281fcc45f89
Mahdi <mahdibagherichanel@gmail.com>
2026-05-30 13:02:25 +0330

    feat: add useDriverAnalysis hook for data fetching

--- apps/api/src/modules/dashboard-driver/modules/driver/driver.controller.ts
+++ apps/api/src/modules/dashboard-driver/modules/driver/driver.controller.ts
@@ -1,45 +1,9 @@
 import { ProfileCompleteGuard } from '@core/dashboard-driver/guards/profile-complete.guard';
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { DriverService } from './driver.service';
-import { CreateDriverDto } from './dto/create-driver.dto';
-import { UpdateDriverDto } from './dto/update-driver.dto';
 
 @Controller('driver')
 @UseGuards(ProfileCompleteGuard)
 export class DriverController {
   constructor(private readonly driverService: DriverService) {}
-
-  @Post()
-  create(@Body() createDriverDto: CreateDriverDto) {
-    return this.driverService.create(createDriverDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.driverService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.driverService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDriverDto: UpdateDriverDto) {
-    return this.driverService.update(+id, updateDriverDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.driverService.remove(+id);
-  }
 }
