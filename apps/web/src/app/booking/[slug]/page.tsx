17dbb8d92de09b7a18d00db4777b4b799e39cf38
mahdi-i <mahdi1750idham@gmail.com>
2026-06-16 09:10:53 +0330

    fix ui booking page with streming

--- apps/web/src/app/booking/[slug]/page.tsx
+++ apps/web/src/app/booking/[slug]/page.tsx
@@ -1,11 +1,36 @@
 import SectionLayout from "@/core/components/custom/ui/wrapper/SectionLayout";
+import SingleBookingPage from "@/core/features/booking/components/blocks/booking/SingleBookingPage";
+import { BASE_URL } from "@/core/lib/basic-link/BackendBasicLink";
+import { getAccessTokenSSR } from "@/core/lib/coockie/getAccess";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: Promise<{
+    slug: string;
+  }>;
 }) {
   const { slug } = await params;
-  console.log(slug);
-  return <SectionLayout>{slug}</SectionLayout>;
+  const driverId = slug;
+  const license = await getAccessTokenSSR();
+
+  const res = await fetch(
+    `${BASE_URL}/booking/drivers-booking-profile/${driverId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${license}`,
+      },
+    },
+  );
+  if (!res.ok) {
+    return notFound();
+  }
+  const data = await res.json();
+  return (
+    <SectionLayout>
+      <SingleBookingPage license={license} driverId={driverId} data={data} />
+    </SectionLayout>
+  );
 }
