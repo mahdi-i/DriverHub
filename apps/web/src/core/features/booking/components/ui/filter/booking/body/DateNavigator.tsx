00be9b2ac476041f643b660cd7b7ed7e55ff0621
mahdi-i <mahdi1750idham@gmail.com>
2026-06-16 09:08:58 +0330

    refactor DateNavigator for select day

--- apps/web/src/core/features/booking/components/ui/filter/booking/body/DateNavigator.tsx
+++ apps/web/src/core/features/booking/components/ui/filter/booking/body/DateNavigator.tsx
@@ -1,12 +1,48 @@
+"use client";
 import { TypographySmall } from "@/core/components/custom/ui/typography/Typography";
 import { Button } from "@/core/components/shadcn/ui/button/button";
+import { formatDate } from "@/core/utils/formatDate";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
 
 interface DateNavigatorProps {
   className?: string;
+  onDateChange?: (date: Date) => void;
 }
 
-export function DateNavigator({ className = "" }: DateNavigatorProps) {
+export function DateNavigator({
+  className = "",
+  onDateChange,
+}: DateNavigatorProps) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const [currentDate, setCurrentDate] = useState(today);
+
+  const goToNextDay = () => {
+    const nextDay = new Date(currentDate);
+    nextDay.setDate(currentDate.getDate() + 1);
+    setCurrentDate(nextDay);
+    onDateChange?.(nextDay);
+  };
+  const goToPrevDay = () => {
+    if (isSameDay(currentDate, today)) {
+      return;
+    }
+
+    const prevDay = new Date(currentDate);
+    prevDay.setDate(currentDate.getDate() - 1);
+    setCurrentDate(prevDay);
+    onDateChange?.(prevDay);
+  };
+
+  const isSameDay = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
   return (
     <div
       className={`flex items-center gap-2 bg-white border border-border rounded-full p-1  ${className}`}
@@ -14,14 +50,19 @@ export function DateNavigator({ className = "" }: DateNavigatorProps) {
       <Button
         variant="ghost"
         size="icon"
+        onClick={goToPrevDay}
+        disabled={isSameDay(currentDate, today)}
         className="h-8 w-8 rounded-full hover:bg-muted"
       >
         <ChevronRightIcon className="h-4 w-4" />
       </Button>
-      <TypographySmall className="px-2 font-medium">29/02/1405</TypographySmall>
+      <TypographySmall className="px-2 font-medium">
+        {formatDate(currentDate.toLocaleDateString())}
+      </TypographySmall>
       <Button
         variant="ghost"
         size="icon"
+        onClick={goToNextDay}
         className="h-8 w-8 rounded-full hover:bg-muted"
       >
         <ChevronLeftIcon className="h-4 w-4" />
@@ -30,7 +71,38 @@ export function DateNavigator({ className = "" }: DateNavigatorProps) {
   );
 }
 
-export function DateNavigatorMobile({ className = "" }: DateNavigatorProps) {
+export function DateNavigatorMobile({
+  className = "",
+  onDateChange,
+}: DateNavigatorProps) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const [currentDate, setCurrentDate] = useState(today);
+
+  const goToNextDay = () => {
+    const nextDay = new Date(currentDate);
+    nextDay.setDate(currentDate.getDate() + 1);
+    setCurrentDate(nextDay);
+    onDateChange?.(nextDay);
+  };
+  const goToPrevDay = () => {
+    if (isSameDay(currentDate, today)) {
+      return;
+    }
+
+    const prevDay = new Date(currentDate);
+    prevDay.setDate(currentDate.getDate() - 1);
+    setCurrentDate(prevDay);
+    onDateChange?.(prevDay);
+  };
+
+  const isSameDay = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
   return (
     <div
       className={`flex items-center gap-2 bg-white border border-border rounded-full p-1  ${className}`}
@@ -38,14 +110,19 @@ export function DateNavigatorMobile({ className = "" }: DateNavigatorProps) {
       <Button
         variant="ghost"
         size="icon"
+        onClick={goToPrevDay}
+        disabled={isSameDay(currentDate, today)}
         className="h-5 w-5 rounded-full hover:bg-muted"
       >
         <ChevronRightIcon className="h-4 w-4" />
       </Button>
-      <TypographySmall className="px-1 font-medium">29/02</TypographySmall>
+      <TypographySmall className="px-1 font-medium">
+        {formatDate(currentDate.toLocaleDateString())}
+      </TypographySmall>
       <Button
         variant="ghost"
         size="icon"
+        onClick={goToNextDay}
         className="h-5 w-5 rounded-full hover:bg-muted"
       >
         <ChevronLeftIcon className="h-4 w-4" />
